a668f087a2dba1d7b2d94735dae3ebe2df0a3c1e
Shekhar Kumar <shkr938@gmail.com>
2020-12-29 08:39:52 +0530

    Add project files.

---
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query= @" 
+                     select  DepartmentId , DepartmentName from
+                     dbo.Department
+                      ";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return  Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+            
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string Department { get; set; }
+
+        public string DateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
